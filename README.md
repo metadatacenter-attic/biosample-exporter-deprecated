1e27ecae4cde51cbe5d01fc10bbe39ab76302ed1
martinjoconnor <martin.oconnor@stanford.edu>
2016-04-01 15:29:48 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+CEDAR 2 BioSample Convertor
+===========================
+
+#### Building and Running
+
+To build this library you must have the following items installed:
+
++ [Java 8](http://www.oracle.com/technetwork/java/javase/downloads/index.html)
++ A tool for checking out a [Git](http://git-scm.com/) repository.
++ Apache's [Maven](http://maven.apache.org/index.html).
+
+Get a copy of the latest code:
+
+    git clone https://github.com/metadatacenter/biosample-exporter.git
+
+Change into the biosample-exporter directory:
+
+    cd biosample-exporter 
+
+Then build it with Maven:
+
+    mvn clean install
+
+To run:
+
+    mvn exec:java
+
---
+++ src/main/java/org/metadatacenter/biosample/exporter/CEDAR2BioSample.java
@@ -0,0 +1,24 @@
+package org.metadatacenter.biosample.exporter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+
+public class CEDAR2BioSample
+{
+  public static void main(String[] argc) throws IOException
+  {
+    ObjectMapper mapper = new ObjectMapper();
+    File addressJSONFile = new File(CEDAR2BioSample.class.getClassLoader().getResource("./json/address.json").getFile());
+
+    Address address1 = mapper.readValue("{\"post-office-box\": \"232\", \"extended-address\": \"an extended\",  " +
+      "\"street-address\": \"123 Main St\", \"locality\": \"a locality\", " +
+      "\"region\": \"a region\", \"postal-code\": \"666\", \"country-name\": \"USA\" }", Address.class);
+
+    Address address2 = mapper.readValue(addressJSONFile, Address.class);
+
+    System.out.println("Address: " + address1);
+    System.out.println("Address: " + address2);
+  }
+}
