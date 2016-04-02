e592290cfba9dca6bb85e15463f276f205854c4a
martinjoconnor <martin.oconnor@stanford.edu>
2016-04-01 17:04:20 -0700

    Added a marshaller

--- src/main/java/org/metadatacenter/biosample/exporter/CEDAR2BioSample.java
+++ src/main/java/org/metadatacenter/biosample/exporter/CEDAR2BioSample.java
@@ -1,15 +1,20 @@
 package org.metadatacenter.biosample.exporter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import generated.ObjectFactory;
 import generated.TypeBioSample;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 import java.io.File;
 import java.io.IOException;
 
 public class CEDAR2BioSample
 {
-  public static void main(String[] argc) throws IOException
+  public static void main(String[] argc) throws IOException, JAXBException
   {
+    ObjectFactory objectFactory = new ObjectFactory();
     ObjectMapper mapper = new ObjectMapper();
     File addressJSONFile = new File(CEDAR2BioSample.class.getClassLoader().getResource("./json/address.json").getFile());
 
@@ -22,6 +27,11 @@ public class CEDAR2BioSample
     System.out.println("Address: " + address1);
     System.out.println("Address: " + address2);
 
-    TypeBioSample bioSample;
+    TypeBioSample bioSample = objectFactory.createTypeBioSample();
+
+    JAXBContext ctx = JAXBContext.newInstance(TypeBioSample.class);
+    Marshaller marshaller = ctx.createMarshaller();
+    marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+    marshaller.marshal(bioSample, System.out);
   }
 }
