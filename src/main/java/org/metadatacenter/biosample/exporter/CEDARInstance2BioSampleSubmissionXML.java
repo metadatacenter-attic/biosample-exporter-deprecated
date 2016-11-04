f69995f1c3fa028b92e7d805481c545c930c5762
martinjoconnor <martin.oconnor@stanford.edu>
2016-11-03 17:05:10 -0700

    Simplified generation

--- src/main/java/org/metadatacenter/biosample/exporter/CEDARInstance2BioSampleSubmissionXML.java
+++ src/main/java/org/metadatacenter/biosample/exporter/CEDARInstance2BioSampleSubmissionXML.java
@@ -1,9 +1,8 @@
 package org.metadatacenter.biosample.exporter;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import common.sp.TypeBlock;
 import common.sp.TypeDescriptor;
-import common.sp.TypeExternalLink;
 import common.sp.TypeOrganism;
 import common.sp.TypePrimaryId;
 import common.sp.TypeRefId;
@@ -24,6 +23,7 @@ import javax.xml.datatype.DatatypeFactory;
 import javax.xml.datatype.XMLGregorianCalendar;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.GregorianCalendar;
 import java.util.List;
 
@@ -33,15 +33,133 @@ public class CEDARInstance2BioSampleSubmissionXML
   {
     ObjectMapper mapper = new ObjectMapper();
 
-    File submissionInstanceJSONFile = new File(CEDARInstance2BioSampleSubmissionXML.class.getClassLoader()
+    mapper.setSerializationInclusion(JsonInclude.Include.ALWAYS);
+
+      File submissionInstanceJSONFile = new File(CEDARInstance2BioSampleSubmissionXML.class.getClassLoader()
       .getResource("./json/NCBIBioSampleSubmissionInstance1.json").getFile());
 
-    NCBIBioSampleSubmissionTemplate bioSampleSubmissionInstance = mapper
+    NCBIBioSampleSubmissionTemplate bioSampleSubmission = mapper
       .readValue(submissionInstanceJSONFile, NCBIBioSampleSubmissionTemplate.class);
 
-    NCBIBioSampleSubmission submission = extractBioSampleSubmissionFromCEDARInstance(bioSampleSubmissionInstance);
+    generateNCBIBioSampleSubmissionXML(bioSampleSubmission);
+  }
 
-    generateNCBIBioSampleSubmissionXML(submission);
+  private static void generateNCBIBioSampleSubmissionXML(NCBIBioSampleSubmissionTemplate bioSampleSubmission)
+    throws DatatypeConfigurationException, JAXBException
+  {
+    generated.ObjectFactory objectFactory = new generated.ObjectFactory();
+    common.sp.ObjectFactory spCommonObjectFactory = new common.sp.ObjectFactory();
+
+    TypeSubmission xmlSubmission = objectFactory.createTypeSubmission();
+    // schema_version="2.0"
+    // xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    // xsi:noNamespaceSchemaLocation="http://www.ncbi.nlm.nih.gov/viewvc/v1/trunk/submit/public-docs/common/submission.xsd?view=co"
+
+    // Submission/Description/Comment
+    TypeSubmission.Description description = objectFactory.createTypeSubmissionDescription();
+    xmlSubmission.setDescription(description);
+    description.setComment("Example CEDAR-generated BioSample submission using the Human.1.0 package");
+
+    // Submission/Description/Hold/releaseDate
+    TypeSubmission.Description.Hold hold = objectFactory.createTypeSubmissionDescriptionHold();
+    description.setHold(hold);
+    hold.setReleaseDate(createXMLGregorianCalendar("2016-10-10"));
+
+    // Submission/Description/Organization
+    TypeOrganization organization = objectFactory.createTypeOrganization();
+    description.getOrganization().add(organization);
+    organization.setRole("master");
+    organization.setType("institute");
+
+    // Submission/Description/Organization/Name
+    TypeOrganization.Name organizationName = objectFactory.createTypeOrganizationName();
+    organization.setName(organizationName);
+    organizationName.setValue("CEDAR");
+
+    // Submission/Description/Organization/ContactInfo/email
+    TypeContactInfo contactInfo = objectFactory.createTypeContactInfo();
+    organization.getContact().add(contactInfo);
+    contactInfo.setEmail("metadatacenter@gmail.com");
+
+    // Submission/Description/Organization/ContactInfo/Name
+    TypeName name = objectFactory.createTypeName();
+    contactInfo.setName(name);
+    name.setFirst("Mr.");
+    name.setLast("CEDAR");
+
+    // Submission/Action
+    TypeSubmission.Action action = objectFactory.createTypeSubmissionAction();
+    xmlSubmission.getAction().add(action);
+
+    // Submission/Action/AddData/target_db
+    TypeSubmission.Action.AddData addData = objectFactory.createTypeSubmissionActionAddData();
+    action.setAddData(addData);
+    addData.setTargetDb("BioSample");
+
+    // Submission/Action/AddData/Data/content_type
+    TypeSubmission.Action.AddData.Data data = objectFactory.createTypeSubmissionActionAddDataData();
+    addData.getData().add(data);
+    data.setContentType("XML");
+
+    // Submission/Action/AddData/Data/XMLContent
+    TypeSubmission.Action.AddData.Data.XmlContent xmlContent = objectFactory.createTypeInlineDataXmlContent();
+    data.setXmlContent(xmlContent);
+
+    // Submission/Action/AddData/Data/XMLContent/BioSample/schema_version
+    TypeBioSample bioSample = objectFactory.createTypeBioSample();
+    xmlContent.setBioSample(bioSample);
+    bioSample.setSchemaVersion("2.0");
+
+    // Submission/Action/AddData/Data/XMLContent/BioSample/SampleID
+    TypeBioSampleIdentifier sampleID = objectFactory.createTypeBioSampleIdentifier();
+    bioSample.setSampleId(sampleID);
+
+    // Submission/Action/AddData/Data/XMLContent/BioSample/SampleID/SPUID
+    TypeBioSampleIdentifier.SPUID spuid = objectFactory.createTypeBioSampleIdentifierSPUID();
+    spuid.setSpuidNamespace("CEDAR");
+    spuid.setValue("XXX"); // TODO Sample ID
+
+    // Submission/Action/AddData/Data/XMLContent/BioSample/Descriptor
+    TypeDescriptor descriptor = spCommonObjectFactory.createTypeDescriptor();
+    bioSample.setDescriptor(descriptor);
+    descriptor.setTitle("Example CEDAR-generated BioSample submission using the Human.1.0 package");
+
+    // Submission/Action/AddData/Data/XMLContent/BioSample/Organism
+    TypeOrganism organism = spCommonObjectFactory.createTypeOrganism();
+    bioSample.setOrganism(organism);
+    organism.setOrganismName("XXX"); // TODO Organism
+
+    // Submission/Action/AddData/Data/XMLContent/BioSample/BioProject
+    TypeRefId bioProject = spCommonObjectFactory.createTypeRefId();
+    bioSample.getBioProject().add(bioProject);
+
+    // Submission/Action/AddData/Data/XMLContent/BioSample/BioProject/PrimaryID
+    TypePrimaryId bioProjectPrimaryID = spCommonObjectFactory.createTypePrimaryId();
+    bioProject.setPrimaryId(bioProjectPrimaryID);
+    bioProjectPrimaryID.setDb("BioProject");
+    bioProjectPrimaryID.setValue("PRJNA212117");
+
+    // Submission/Action/AddData/Data/XMLContent/BioSample/Package
+    bioSample.setPackage("Human.1.0");
+
+    // Submission/Action/AddData/Data/XMLContent/BioSample/Attributes
+    TypeBioSample.Attributes attributes = objectFactory.createTypeBioSampleAttributes();
+    bioSample.setAttributes(attributes);
+
+    // Submission/Action/AddData/Data/XMLContent/BioSample/Attributes/Attribute
+    for (NCBIBioSampleSubmission.Attribute bioSampleAttribute : new ArrayList<NCBIBioSampleSubmission.Attribute>()) { // TODO attributes
+      TypeAttribute attribute = objectFactory.createTypeAttribute();
+      attributes.getAttribute().add(attribute);
+      attribute.setAttributeName(bioSampleAttribute.getAttributeName());
+      attribute.setValue(bioSampleAttribute.getAttributeValue());
+    }
+
+    JAXBElement<TypeSubmission> submissionRoot = objectFactory.createSubmission(xmlSubmission);
+
+    JAXBContext ctx = JAXBContext.newInstance(TypeSubmission.class);
+    Marshaller marshaller = ctx.createMarshaller();
+    marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+    marshaller.marshal(submissionRoot, System.out);
   }
 
   private static NCBIBioSampleSubmission extractBioSampleSubmissionFromCEDARInstance(
@@ -115,141 +233,6 @@ public class CEDARInstance2BioSampleSubmissionXML
     return bioSampleSubmission;
   }
 
-  private static void generateNCBIBioSampleSubmissionXML(NCBIBioSampleSubmission submission)
-    throws DatatypeConfigurationException, JAXBException
-  {
-    generated.ObjectFactory objectFactory = new generated.ObjectFactory();
-    common.sp.ObjectFactory spCommonObjectFactory = new common.sp.ObjectFactory();
-
-    TypeSubmission xmlSubmission = objectFactory.createTypeSubmission();
-    // schema_version="2.0"
-    // xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-    // xsi:noNamespaceSchemaLocation="http://www.ncbi.nlm.nih.gov/viewvc/v1/trunk/submit/public-docs/common/submission.xsd?view=co"
-
-    // Submission/Description
-    TypeSubmission.Description description = objectFactory.createTypeSubmissionDescription();
-    xmlSubmission.setDescription(description);
-    description.setComment(submission.getComment());
-
-    // Submission/Description/Hold
-    TypeSubmission.Description.Hold hold = objectFactory.createTypeSubmissionDescriptionHold();
-    description.setHold(hold);
-    hold.setReleaseDate(createXMLGregorianCalendar(submission.getReleaseDate()));
-
-    // Submission/Description/Organization
-    TypeOrganization organization = objectFactory.createTypeOrganization();
-    description.getOrganization().add(organization);
-    organization.setRole(submission.getOrganizationRole());
-    organization.setType(submission.getOrganizationType());
-
-    // Submission/Description/Organization/Name
-    TypeOrganization.Name organizationName = objectFactory.createTypeOrganizationName();
-    organization.setName(organizationName);
-    organizationName.setValue(submission.getOrganizationName());
-
-    // Submission/Description/Organization/ContactInfo
-    TypeContactInfo contactInfo = objectFactory.createTypeContactInfo();
-    organization.getContact().add(contactInfo);
-    contactInfo.setEmail(submission.getOrganizationContactEmailAddress());
-
-    // Submission/Description/Organization/ContactInfo/Name
-    TypeName name = objectFactory.createTypeName();
-    contactInfo.setName(name);
-    name.setFirst(submission.getOrganizationContactFirstName());
-    name.setMiddle(submission.getOrganizationContactMiddleInitial());
-    name.setLast(submission.getOrganizationContactLastName());
-
-    // Submission/Action
-    TypeSubmission.Action action = objectFactory.createTypeSubmissionAction();
-    xmlSubmission.getAction().add(action);
-
-    // Submission/Action/AddData
-    TypeSubmission.Action.AddData addData = objectFactory.createTypeSubmissionActionAddData();
-    action.setAddData(addData);
-    addData.setTargetDb(submission.getTargetDatabase());
-
-    // Submission/Action/AddData/Identifier?
-    //    <Identifier><SPUID spuid_namespace="Institute Name">Unique submitter identifier</SPUID></Identifier>
-
-    // Submission/Action/AddData/Data
-    TypeSubmission.Action.AddData.Data data = objectFactory.createTypeSubmissionActionAddDataData();
-    addData.getData().add(data);
-    data.setContentType(submission.getBioSampleSubmissionContentType());
-
-    // Submission/Action/AddData/Data/XMLContent
-    TypeSubmission.Action.AddData.Data.XmlContent xmlContent = objectFactory.createTypeInlineDataXmlContent();
-    data.setXmlContent(xmlContent);
-
-    // Submission/Action/AddData/Data/XMLContent/BioSample
-    TypeBioSample bioSample = objectFactory.createTypeBioSample();
-    xmlContent.setBioSample(bioSample);
-    bioSample.setSchemaVersion(submission.getBioSampleSchemaVersion());
-    // xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-    // xsi:noNamespaceSchemaLocation=submission.getBioSampleSchemaLocation();
-
-    // Submission/Action/AddData/Data/XMLContent/BioSample/SampleID
-    TypeBioSampleIdentifier sampleID = objectFactory.createTypeBioSampleIdentifier();
-    bioSample.setSampleId(sampleID);
-
-    // Submission/Action/AddData/Data/XMLContent/BioSample/SampleID/SPUID
-    TypeBioSampleIdentifier.SPUID spuid = objectFactory.createTypeBioSampleIdentifierSPUID();
-    sampleID.getSPUID().add(spuid);
-    spuid.setSpuidNamespace(submission.getBioSampleSampleIDSPUIDNamespace());
-    spuid.setValue(submission.getBioSampleSampleIDSPUIDSubmitterIdentifier());
-
-    // Submission/Action/AddData/Data/XMLContent/BioSample/Descriptor
-    TypeDescriptor descriptor = spCommonObjectFactory.createTypeDescriptor();
-    bioSample.setDescriptor(descriptor);
-    descriptor.setTitle(submission.getBioSampleDescriptorTitle());
-
-    // Submission/Action/AddData/Data/XMLContent/BioSample/Descriptor/Description
-    TypeBlock descriptorDescription = spCommonObjectFactory.createTypeBlock();
-    descriptor.setDescription(descriptorDescription);
-    //descriptorDescription.getPOrUlOrOl().add(submission.getBioSampleDescriptorDescription())
-
-    // Submission/Action/AddData/Data/XMLContent/BioSample/Descriptor/ExternalLink
-    TypeExternalLink externalLink = spCommonObjectFactory.createTypeExternalLink();
-    descriptor.getExternalLink().add(externalLink);
-    externalLink.setLabel(submission.getBioSampleDescriptorExternalLinkLabel());
-    externalLink.setURL(submission.getBioSampleDescriptorExternalLinkURL());
-
-    // Submission/Action/AddData/Data/XMLContent/BioSample/Organism
-    TypeOrganism organism = spCommonObjectFactory.createTypeOrganism();
-    bioSample.setOrganism(organism);
-    organism.setOrganismName(submission.getBioSampleOrganismName());
-
-    // Submission/Action/AddData/Data/XMLContent/BioSample/BioProject
-    TypeRefId bioProject = spCommonObjectFactory.createTypeRefId();
-    bioSample.getBioProject().add(bioProject);
-
-    // Submission/Action/AddData/Data/XMLContent/BioSample/BioProject/PrimaryID
-    TypePrimaryId bioProjectPrimaryID = spCommonObjectFactory.createTypePrimaryId();
-    bioProject.setPrimaryId(bioProjectPrimaryID);
-    bioProjectPrimaryID.setDb(submission.getBioSampleBioProjectPrimaryIDDatabase());
-    bioProjectPrimaryID.setValue(submission.getBioSampleBioProjectPrimaryID());
-
-    // Submission/Action/AddData/Data/XMLContent/BioSample/Package
-    bioSample.setPackage(submission.getBioSamplePackageID());
-
-    // Submission/Action/AddData/Data/XMLContent/BioSample/Attributes
-    TypeBioSample.Attributes attributes = objectFactory.createTypeBioSampleAttributes();
-    bioSample.setAttributes(attributes);
-
-    // Submission/Action/AddData/Data/XMLContent/BioSample/Attributes/Attribute
-    for (NCBIBioSampleSubmission.Attribute bioSampleAttribute : submission.getAttributes()) {
-      TypeAttribute attribute = objectFactory.createTypeAttribute();
-      attributes.getAttribute().add(attribute);
-      attribute.setAttributeName(bioSampleAttribute.getAttributeName());
-      attribute.setValue(bioSampleAttribute.getAttributeValue());
-    }
-
-    JAXBElement<TypeSubmission> submissionRoot = objectFactory.createSubmission(xmlSubmission);
-
-    JAXBContext ctx = JAXBContext.newInstance(TypeSubmission.class);
-    Marshaller marshaller = ctx.createMarshaller();
-    marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-    marshaller.marshal(submissionRoot, System.out);
-  }
 
   private static XMLGregorianCalendar createXMLGregorianCalendar(String date) throws DatatypeConfigurationException
   {
